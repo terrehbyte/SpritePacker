6fb9fd84b0280d91c46d9622573505ff02db4f88
Terry Nguyen <terreh@terrehbyte.com>
2014-05-01 17:44:49 -0700

    Added comments to ImageIO.cs

--- SpritePacker/ImageIO.cs
+++ SpritePacker/ImageIO.cs
@@ -12,6 +12,8 @@ using System.Text.RegularExpressions;
 
 namespace SpritePacker
 {
+    // consider rewriting this class to require instantiation
+    // for more control over dialog options
     static class ImageIO
     {
         // - Filter Parameters -
@@ -23,7 +25,13 @@ namespace SpritePacker
         // Used to build Win Open/SaveDialog filters
         private static string buildFilterStr(string customFilter)
         {
+            // create an empty string to append things to
             string internalFilter = "";
+
+            // @terrehbyte: consider instead looping over the string
+            // and appending based on the current letter
+            // so the developer can decide what order they want to
+            // set the filters up as
 
             // PNG
             if (Regex.IsMatch(customFilter, "p", RegexOptions.IgnoreCase))
@@ -62,19 +70,24 @@ namespace SpritePacker
         {
             // Create a save file dialog
             Microsoft.Win32.SaveFileDialog saveDiag = new Microsoft.Win32.SaveFileDialog();
-            saveDiag.FileName = "";
-            saveDiag.Filter = ImageIO.buildFilterStr(filter);
-            saveDiag.FilterIndex = 0;
+            saveDiag.FileName = "";                           // default file name
+            saveDiag.Filter = ImageIO.buildFilterStr(filter); // string for filter
+            saveDiag.FilterIndex = 0;                         // Which filter is the default?
 
-            // Show the dialog
+            // Show the dialog and store whether or not the user selected something
+            //  false means cancel
+            //  true means selection occurred
             Nullable<bool> diagResult = saveDiag.ShowDialog();
 
+            // Return file path if true, otherwise return null
             if (diagResult == true)
             {
-                return saveDiag.FileName;
+                return saveDiag.FileName;   // this is actually the full/absolute string for file path
+                                            // (i.e. "C:\Users\Student\Pictures\testImage.png")
             }
             else
             {
+                // other program should handle null
                 return null;
             }
         }
@@ -82,14 +95,18 @@ namespace SpritePacker
         // Opens a Windows Open Dialog
         public static string OpenDialog(string filter = "pjb")
         {
+            // Create an OpenFile Dialgo
             Microsoft.Win32.OpenFileDialog openDiag = new Microsoft.Win32.OpenFileDialog();
-            openDiag.Filter = ImageIO.buildFilterStr(filter);
+            openDiag.Multiselect = true;    // allow the user to select more than one thing
+            openDiag.Filter = ImageIO.buildFilterStr(filter);   // build the string for the filter
 
             Nullable<bool> diagResult = openDiag.ShowDialog();
 
+            // Return filename if true, otherwise return null
             if (diagResult == true)
             {
-                return openDiag.FileName;
+                return openDiag.FileName;   // this is actually the full/absolute string for file path
+                                            // (i.e. "C:\Users\Student\Pictures\testImage.png")
             }
             else
             {
@@ -130,12 +147,15 @@ namespace SpritePacker
                     }
             }
 
-
+            // Add the BitmapImage to the encoder's frame queue
             imageEnc.Frames.Add(BitmapFrame.Create(newImage));
 
             // Open a file stream to save things to
+            //  @terrehbyte: NEEDS error handling based on exceptions
+            //  @terrehbyte: test for file access (see SO: http://bit.ly/1jmIiRB )
             FileStream saveStream = new FileStream(filePathName, FileMode.Create);
 
+            // Save into the stream
             imageEnc.Save(saveStream);
             saveStream.Close(); // always close the stream when you're done
         }
